df287660fb719ef1dca839709b71c973166b79f9
raghavt20 <raghavtandon2000@gmail.com>
2021-06-30 15:09:18 +0530

    phoenix: Use common QTI vibrator component
    * also build CarrierConfigOverlay for now
    
    Change-Id: Id41be46e178e4ec41ce96a287b45da7578ee3b80

--- device.mk
+++ device.mk
@@ -327,7 +327,12 @@ TARGET_COMMON_QTI_COMPONENTS := \
     bt \
     perf \
     telephony \
-    usb
+    usb \
+    vibrator
+
+# RIL
+PRODUCT_PACKAGES += \
+    CarrierConfigOverlay
 
 # Recovery
 PRODUCT_PACKAGES += \
@@ -394,9 +399,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
