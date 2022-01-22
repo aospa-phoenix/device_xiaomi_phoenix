5c599f8bc90c886b7cf6cc9c1ff4abfaaf754394
Alexander Koskovich <akoskovich@pm.me>
2022-01-22 01:44:38 -0700

    phoenix: Move ueventd.qcom.rc to /vendor/etc.
     * This is the preferred location as of Android 12.
    
    Change-Id: I8c7d2230281b6d8da802bf10f88461289849acaf

--- device.mk
+++ device.mk
@@ -308,7 +308,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Uevent
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/ueventd.rc
+    $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
 
 # Vibrator
 PRODUCT_COPY_FILES += \
