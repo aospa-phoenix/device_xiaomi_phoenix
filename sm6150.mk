479d570985cd3a58f5910be7c11dd57f7a207d6a
Gagan Malvi <malvigagan@gmail.com>
2022-09-12 12:54:16 +0200

    Revert "sm6150-common: Don't inherit updatable_apex.mk"
    This reverts commit 3c756fa19fd6522ab759bc39539e8df7810d027e.

--- sm6150.mk
+++ sm6150.mk
@@ -31,6 +31,9 @@ MSMSTEPPE := sm6150
 PRODUCT_USES_QCOM_HARDWARE := true
 PRODUCT_BOARD_PLATFORM := $(MSMSTEPPE)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
