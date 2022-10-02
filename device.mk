607fd1f18e0e16d8e773e30d76e10386c86cc95f
Gagan Malvi <malvigagan@gmail.com>
2022-10-02 14:25:59 +0200

    phoenix: lights: Use common hardware/xiaomi implementation.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -242,7 +242,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_sm6150
+    android.hardware.light-service.xiaomi
 
 # Media
 PRODUCT_PACKAGES += \
