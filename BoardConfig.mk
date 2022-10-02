dba3eb656de8a4e9c01206ff326713c1d7548695
Gagan Malvi <malvigagan@gmail.com>
2022-10-02 17:36:59 +0200

    phoenix: Use media-legacy component.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -75,7 +75,6 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     vendor/qcom/opensource/core-utils/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/hidl/device_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
-DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
 ODM_MANIFEST_SKUS += phoenix
 ODM_MANIFEST_PHOENIX_FILES := \
