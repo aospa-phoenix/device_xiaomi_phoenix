e8e9ec9f03a63803d013b0f192dcafde54f62500
Gagan Malvi <malvigagan@gmail.com>
2022-10-03 05:08:53 +0200

    phoenix: Use default kernel path.
    * QCOM defaults to kernel/msm-4.14.
    
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -91,8 +91,6 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sm6150
-
 BOARD_KERNEL_CMDLINE += androidboot.hardware=qcom
 BOARD_KERNEL_CMDLINE += androidboot.usbcontroller=a600000.dwc3
 ifeq ($(PHOENIX_DEBUG),true)
@@ -105,7 +103,6 @@ BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += androidboot.memcg=1 cgroup.memory=nokmem,nosocket
 BOARD_KERNEL_CMDLINE += kpti=off
-KERNEL_LD := LD=ld.lld
 
 # TARGET_KERNEL_APPEND_DTB handling
 TARGET_KERNEL_APPEND_DTB := false
