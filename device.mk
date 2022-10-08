3df4cbfbef7a1992533708b12996194e2001ebe4
Gagan Malvi <malvigagan@gmail.com>
2022-10-08 11:49:42 +0200

    phoenix: refactor and rearrange overlays.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -344,8 +344,8 @@ PRODUCT_COPY_FILES += \
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-device
+    $(LOCAL_PATH)/overlay/common \
+    $(LOCAL_PATH)/overlay/device
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
