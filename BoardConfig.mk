d33fe66313b727cde6ac99e0e3d7d16214638173
Gagan Malvi <malvigagan@gmail.com>
2022-10-17 20:31:34 +0200

    phoenix: hidl: Improve readability.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -67,8 +67,10 @@ LOC_HIDL_VERSION := 4.0
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/hidl/device_framework_compatibility_matrix.xml
+
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/configs/hidl/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
+
 ODM_MANIFEST_SKUS += phoenix
 ODM_MANIFEST_PHOENIX_FILES := \
      $(DEVICE_PATH)/configs/hidl/manifest-nfc.xml
