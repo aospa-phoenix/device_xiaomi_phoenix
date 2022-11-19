74a1f928a07d964eaf212accbcdde521f835532d
Gagan Malvi <malvigagan@gmail.com>
2022-11-19 14:44:48 +0100

    phoenix: rootdir: Separate out performance changes into another rc.
    Change-Id: I751b8940c4cdaa74f7d328cb3e44ee3b5e35ddb5
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- device.mk
+++ device.mk
@@ -152,7 +152,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.rc \
     $(LOCAL_PATH)/rootdir/etc/init.qcom.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.qcom.usb.rc \
-    $(LOCAL_PATH)/rootdir/etc/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc
+    $(LOCAL_PATH)/rootdir/etc/init.target.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.target.rc \
+    $(LOCAL_PATH)/rootdir/etc/init.phoenix.performance.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.phoenix.performance.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/bin/init.qcom.post_boot.sh:$(TARGET_COPY_OUT_VENDOR)/bin/init.qcom.post_boot.sh \
@@ -231,7 +232,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/init.phoenix.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.phoenix.rc
+    $(LOCAL_PATH)/rootdir/etc/init.phoenix.nfc.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.phoenix.nfc.rc
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_phoenix/android.hardware.nfc.xml \
